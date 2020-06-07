ee26a3212ddcff4f595920f6d0b0e27470365897
danelmacdonald <55730975+danelmacdonald@users.noreply.github.com>
2020-06-07 22:09:44 +0200

    Implemeted most of Signup/Login API functionality

--- GymMoves_WebAPI/Controllers/UserManagementController.cs
+++ GymMoves_WebAPI/Controllers/UserManagementController.cs
@@ -1,87 +1,134 @@
-using GymMoves_WebAPI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
-using System.Web.Http;
+﻿using GymMoves_WebAPI.Data.EntityModels;
+using GymMoves_WebAPI.Data.Repositories;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GymMoves_WebAPI.Data.Repositories.Implementation;
 
 namespace GymMoves_WebAPI.Controllers
 {
-    public class UserManagementController : ApiController
+    [ApiController]
+    public class UserManagementController : Controller
     {
-        [Route("api/signup")]
-        [HttpPost]
-        public HttpResponseMessage SignUp(Users user)
+
+        private readonly UserRepositoryInterface _repo;
+
+        public UserManagementController(UserRepositoryInterface repo)
         {
+            _repo = repo;
+        }
 
-            Boolean successful = false;
+    
 
 
+        [Route("api/signup/user")]
+        [HttpPost]
+        public async Task<ActionResult<UserEntity>> SignUpUser(UserEntity user)
+        {
+            bool added = false;
 
-            if (user.ID == 123)
+            
+            added = await _repo.Add(user);
+           
+            if (added)
             {
-                successful = true;
-            }
-
-
-            if (successful)
-            {
-                string returnMessage = "{ \"Successful\" : \"true\", \"User Type\" : \"Gym Member\", \"Name\" : \"Sally\"}";
-
-                var response = Request.CreateResponse(HttpStatusCode.OK);
-                response.Content = new StringContent(returnMessage, Encoding.UTF8, "application/json");
-
-
-                return response;
+                return Ok(user);
             }
             else
             {
-                string returnMessage = "{ \"Successful\" : \"false\", \"Reason\" : \"The member ID does not exist!\"}";
-
-                var response = Request.CreateResponse(HttpStatusCode.Unauthorized);
-                response.Content = new StringContent(returnMessage, Encoding.UTF8, "application/json");
-
-
-                return response;
+                return StatusCode(StatusCodes.Status401Unauthorized, "{\"Error\" : \"Failed to find this member.\"}");
             }
         }
 
-        [Route("api/login")]
+        [Route("api/signup/instructor")]
         [HttpPost]
-        public HttpResponseMessage LogIn(Users user)
+        public async Task<ActionResult<InstructorEntity>> SignUp(InstructorEntity user)
         {
+            bool added = false;
 
-            Boolean successful = false;
+            added = await _repo.AddInstructor(user);
 
-
-
-            if (user.password == "password")
+            if (added)
             {
-                successful = true;
-            }
-
-
-            if (successful)
-            {
-                string returnMessage = "{ \"Successful\" : \"true\", \"User Type\" : \"Gym Member\", \"Name\" : \"Sally\"}";
-
-                var response = Request.CreateResponse(HttpStatusCode.OK);
-                response.Content = new StringContent(returnMessage, Encoding.UTF8, "application/json");
-
-
-                return response;
+                return Ok(user);
             }
             else
             {
-                string returnMessage = "{ \"Successful\" : \"false\", \"Reason\" : \"The member ID does not exist!\"}";
+                return StatusCode(StatusCodes.Status401Unauthorized, "{\"Error\" : \"Failed to find this member.\"}");
+            }
+        }
 
-                var response = Request.CreateResponse(HttpStatusCode.Unauthorized);
-                response.Content = new StringContent(returnMessage, Encoding.UTF8, "application/json");
+        [Route("api/signup/manager")]
+        [HttpPost]
+        public async Task<ActionResult<UserEntity>> SignUpManager(UserEntity user)
+        {
+            bool added = false;
+
+            added = await _repo.Add(user);
+
+            if (added)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "{\"Error\" : \"Failed to find this member.\"}");
+            }
+        }
 
 
-                return response;
+
+        [Route("api/login/user")]
+        [HttpPost]
+        public async Task<ActionResult<UserEntity>> LogInUser(UserEntity user)
+        {
+            UserEntity person = null;
+
+            person = await _repo.GetUserWithID(user.UserID);
+
+            if (person != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "{\"Error\" : \"Failed to find this member.\"}");
+            }
+        }
+
+        [Route("api/login/instructor")]
+        [HttpPost]
+        public async Task<ActionResult<InstructorEntity>> LogInInstructor(InstructorEntity user)
+        {
+            InstructorEntity person = null;
+
+            person = await _repo.GetInstructorWithID(user.InstructorID);
+
+            if (person != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "{\"Error\" : \"Failed to find this member.\"}");
+            }
+        }
+
+        [Route("api/login/manager")]
+        [HttpPost]
+        public async Task<ActionResult<UserEntity>> LoginManager(UserEntity user)
+        {
+            UserEntity person = null;
+
+            person = await _repo.GetUserWithID(user.UserID);
+
+            if (person != null)
+            {
+                return Ok(user);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status401Unauthorized, "{\"Error\" : \"Failed to find this member.\"}");
             }
         }
     }
