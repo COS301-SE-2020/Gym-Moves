83210e65e39771d45e8ee1a2116c06cd8bf28e42
danelmacdonald <55730975+danelmacdonald@users.noreply.github.com>
2020-06-10 22:25:20 +0200

    Revert "Merge branch 'WebAPI'"
    This reverts commit d9435ef0f37497dcab2758974742c8857a6eac9d, reversing
    changes made to 2b414a9afd03c4b81b1210b83120b6ea72685fcd.

--- GymMoves_WebAPI/Data/Models/ReturnObjectUM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GymMoves_WebAPI.Data.Models
-{
-    public class ReturnObjectUM
-    {
-        public string status { get; set; }
-        public string user { get; set; }
-        public string message { get; set; }
-    }
-}
