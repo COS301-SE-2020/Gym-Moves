40728d06dcb5d2fd2085afac182870d8532b78af
danelmacdonald <u18008659@tuks.co.za>
2020-07-03 18:55:22 +0200

    Update and rename NotificationsContoller.cs to NotificationsController.cs

--- GymMovesWebAPI/Controllers/NotificationsController.cs
+++ GymMovesWebAPI/Controllers/NotificationsController.cs
@@ -36,15 +36,13 @@ using GymMovesWebAPI.Data.Models.RequestModels;
 using GymMovesWebAPI.Data.Models.ResponseModels;
 using GymMovesWebAPI.Data.Models.DatabaseModels;
 
-namespace GymMovesWebAPI.Controllers
-{
+namespace GymMovesWebAPI.Controllers {
     [ApiController]
-    public class NotificationsController : Controller
-    {
-        private readonly IUserRepository userRepository;
-        private readonly IGymRepository gymRepository;
-        private readonly INotificationSettingsRepository notificationSettingsRepository;
-        private readonly INotificationsRepository notificationsRepository;
+    public class NotificationsController : Controller {
+        private IUserRepository userRepository;
+        private IGymRepository gymRepository;
+        private INotificationSettingsRepository notificationSettingsRepository;
+        private INotificationsRepository notificationsRepository;
 
         public NotificationsController(IUserRepository userRep, INotificationSettingsRepository notificationSettingsRep, INotificationsRepository notificationsRep,
             IGymRepository gymRepo)
@@ -57,7 +55,65 @@ namespace GymMovesWebAPI.Controllers
 
 
         }
+        
+        private void sendGmailEmail(MailMessage message)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Gmail smtp    
+            System.Net.NetworkCredential basicCredential1 = new
+            System.Net.NetworkCredential("tiamangena@gmail.com", "xxuwyymlscsuiwhg");
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = basicCredential1;
+            try
+            {
+                client.Send(message);
+            }
 
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private async Task<bool> addAnnouncement(NotificationRequest req) {
+            DateTime dateTime = new DateTime(int.Parse(req.announcementYear), int.Parse(req.announcementMonth), int.Parse(req.announcementDay));
+            Notifications notif = new Notifications() { Body = req.body, GymIdForeignKey = req.gymId, Heading = req.heading, Date = dateTime };
+
+           return await notificationsRepository.addNotification(notif);
+        }
+
+
+        [Route("api/sendNotification")]
+        [HttpPost]
+        public async Task<ActionResult<NotificationResponse>> sendEmail(NotificationRequest req)
+        {
+   
+            Users[] Members = await gymRepository.getMembers(req.gymId);
+
+            foreach(Users user in Members) {
+
+                if (user.NotificationSetting.Email) { 
+                    string from = "tiamangena@gmail.com"; //From address    
+                    MailMessage message = new MailMessage(from, user.Email);
+
+                    message.Subject = req.heading;
+                    message.Body = req.body;
+                    message.BodyEncoding = Encoding.UTF8;
+                    message.IsBodyHtml = true;
+                    sendGmailEmail(message);
+                }
+            }
+
+            bool sent = await addAnnouncement(req);
+
+            if (sent)
+                return Ok(new NotificationResponse() { message = "Notification added and sent successfully." });
+            else
+                return StatusCode(StatusCodes.Status500InternalServerError, new NotificationResponse() { message = "There was an error storing the announcement. Please try again later" });
+            
+            
+        }
+        
         /*
        Method Name:
           changeNotificationSetting
@@ -79,66 +135,6 @@ namespace GymMovesWebAPI.Controllers
                 return StatusCode(500);
             }
 
-        }
-
-        [Route("api/sendNotification")]
-        [HttpPost]
-        private async Task<ActionResult<NotificationResponse>> sendEmail(NotificationRequest req)
-        {
-
-            Users[] Members = await gymRepository.getMembers(req.gymId);
-
-            foreach (Users user in Members)
-            {
-
-                if (user.NotificationSetting.Email)
-                {
-                    string from = "tiamangena@gmail.com"; //From address    
-                    MailMessage message = new MailMessage(from, user.Email);
-
-                    message.Subject = req.heading;
-                    message.Body = req.body;
-                    message.BodyEncoding = Encoding.UTF8;
-                    message.IsBodyHtml = true;
-                    sendGmailEmail(message);
-                }
-            }
-
-            bool sent = await addAnnouncement(req);
-
-            if (sent)
-                return Ok(new NotificationResponse() { message = "Notification added and sent successfully." });
-            else
-                return StatusCode(StatusCodes.Status500InternalServerError, new NotificationResponse() { message = "There was an error storing the announcement. Please try again later" });
-
-
-        }
-
-        private void sendGmailEmail(MailMessage message)
-        {
-            SmtpClient client = new SmtpClient("smtp.gmail.com", 587); //Gmail smtp    
-            System.Net.NetworkCredential basicCredential1 = new
-            System.Net.NetworkCredential("tiamangena@gmail.com", "xxuwyymlscsuiwhg");
-            client.EnableSsl = true;
-            client.UseDefaultCredentials = false;
-            client.Credentials = basicCredential1;
-            try
-            {
-                client.Send(message);
-            }
-
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-
-        private async Task<bool> addAnnouncement(NotificationRequest req)
-        {
-            DateTime dateTime = new DateTime(int.Parse(req.announcementYear), int.Parse(req.announcementMonth), int.Parse(req.announcementDay));
-            Notifications notif = new Notifications() { Body = req.body, GymIdForeignKey = req.gymId, Heading = req.heading, Date = dateTime };
-
-            return await notificationsRepository.addNotification(notif);
         }
     }
 }
