62a2d527f82fca2faac6ef591433cede8399434f
MarineXII <derpyissunny@gmail.com>
2020-07-05 15:47:22 +0200

    Added test files to test serving web pages

---
+++ GymMovesWebAPI/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymMovesWebAPI.Controllers {
+    public class TestModel {
+        public string tt { get; set; }
+        public int nn { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase {
+        [HttpPost("poof")]
+        public ActionResult<TestModel> poof(TestModel t) {
+            Console.WriteLine("Request...");
+            return Ok(t);
+        }
+    }
+}
