130e1deb8603745fca09333315345b99e622c31b
danelmacdonald <55730975+danelmacdonald@users.noreply.github.com>
2020-07-22 23:48:49 +0200

    Added user controller tests

---
+++ GymMovesWebAPI.Tests/TestDB.cs
@@ -0,0 +1,34 @@
+﻿using GymMovesWebAPI.Data.DatabaseContexts.MainDatabaseContext;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymMovesWebAPI.Tests
+{
+    public abstract class TestWithSqlite : IDisposable
+    {
+        private const string InMemoryConnectionString = "DataSource=:memory:";
+        private readonly SqliteConnection _connection;
+
+        protected readonly MainDatabaseContext DbContext;
+
+        protected TestWithSqlite()
+        {
+            _connection = new SqliteConnection(InMemoryConnectionString);
+            _connection.Open();
+            var options = new DbContextOptionsBuilder<MainDatabaseContext>()
+                    .UseSqlite(_connection)
+                    .Options;
+            DbContext = new MainDatabaseContext(options);
+            DbContext.Database.EnsureCreated();
+        }
+
+        public void Dispose()
+        {
+            _connection.Close();
+        }
+
+    }
+}
