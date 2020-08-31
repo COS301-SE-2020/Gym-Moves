6dccf5cb1383fbaa5f804fe41dae4b1d9af4f7b6
raythedaisy <53380654+raythedaisy@users.noreply.github.com>
2020-08-31 20:25:36 +0200

    Pie chart JS for home page

---
+++ GymMovesWebAPI/wwwroot/assets/js/demo/pie.js
@@ -0,0 +1,61 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+
+var num1 = getCookie("registered").split(",")[0];
+var num2 = getCookie("notregistered").split(",")[0];
+
+function getCookie(cname) {
+
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+
+    return "";
+}
+
+
+
+var myPieChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Registered on app", "Not registered on App"],
+        datasets: [{
+            data: [num1, num2],
+            backgroundColor: ['#7341E6', '#C5ABFF'],
+            hoverBackgroundColor: ['#B762FB', '#E3BCFF'],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+    },
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+            backgroundColor: "rgb(255,255,255)",
+            bodyFontColor: "#858796",
+            borderColor: '#dddfeb',
+            borderWidth: 1,
+            xPadding: 15,
+            yPadding: 15,
+            displayColors: false,
+            caretPadding: 10,
+        },
+        legend: {
+            display: false
+        },
+        cutoutPercentage: 80,
+    },
+});
