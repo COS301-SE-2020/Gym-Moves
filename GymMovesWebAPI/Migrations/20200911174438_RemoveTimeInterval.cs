1e35461017e3975ca5ba91bc63a9f61806eb241c
MarineXII <derpyissunny@gmail.com>
2020-09-11 19:48:41 +0200

    Added migrations

---
+++ GymMovesWebAPI/Migrations/20200911174438_RemoveTimeInterval.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GymMovesWebAPI.Migrations
+{
+    public partial class RemoveTimeInterval : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GymAttendence",
+                table: "GymAttendence");
+
+            migrationBuilder.DropColumn(
+                name: "TimeInterval",
+                table: "GymAttendence");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GymAttendence",
+                table: "GymAttendence",
+                columns: new[] { "GymId", "Date" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GymAttendence",
+                table: "GymAttendence");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TimeInterval",
+                table: "GymAttendence",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GymAttendence",
+                table: "GymAttendence",
+                columns: new[] { "GymId", "Date", "TimeInterval" });
+        }
+    }
+}
