d28bf20ad2f8761b21c511c12e8f75a80673fda1
MarineXII <derpyissunny@gmail.com>
2020-09-11 19:27:21 +0200

    Added interface for gym attendence repositroy

---
+++ GymMovesWebAPI/Data/Repositories/Interfaces/IGymAttendenceRepository.cs
@@ -0,0 +1,13 @@
+﻿using GymMovesWebAPI.Data.Models.DatabaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymMovesWebAPI.Data.Repositories.Interfaces {
+    public interface IGymAttendenceRepository {
+        public Task<bool> addAttendence(GymAttendenceRecord record);
+        public Task<GymAttendenceRecord> getAttendenceRecord(int gymId, DateTime date, string interval);
+        public Task<bool> updateAttendence(GymAttendenceRecord record);
+    }
+}
