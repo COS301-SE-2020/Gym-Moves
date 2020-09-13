f6ee02eba7fe17c739355a775deade019ab8f1be
TiaMangena <u18059903@gmail.com>
2020-09-13 16:46:24 +0200

    updated model to deploy

--- Machine Learning/perceptron.py
+++ Machine Learning/perceptron.py
@@ -6,6 +6,12 @@ import json
 import datetime
 import time
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+import joblib
+from firebase_admin import storage
+
 def getData(gym, xValues, yValues):
     geturl = "https://gymmoveswebapi.azurewebsites.net/api/gymattendance/get"
     getparams = {'gymId' : gym}
@@ -42,17 +48,33 @@ def getClasses(gym):
         week[ind] += 1
     #print(week)
     return week
+
+def getPrediction(mlp, xValues):
+    mlp.predict(xValues)
+
 xValues = list()
 yValues = list()
 getData(3, xValues, yValues)
+mlp = MLPRegressor(solver= 'lbfgs', learning_rate='adaptive', shuffle = False)
+mlp.fit(xValues,yValues)
+
+if (not len(firebase_admin._apps)):
+	cred = credentials.Certificate(r'Machine Learning\sdk.json')
+	firebase_admin.initialize_app(cred, {
+        'storageBucket': 'fbprojid.appspot.com'
+    })
+db = firestore.client()
+
+joblib.dump(mlp, 'model.joblib')
+bucket = storage.bucket(name=)
+b = bucket.blob('model-v1/model.joblib')
+b.upload_from_filename('model.joblib')
+print('model uploaded!')
 
 #X_train, X_test, y_train, y_test = train_test_split(xValues, yValues, test_size=0.30, random_state=40)
 #print(xValues)
 #print(yValues)
 
-mlp = MLPRegressor(solver= 'lbfgs', learning_rate='adaptive', shuffle = False)
-mlp.fit(xValues,yValues)
-
-predict_train = mlp.predict(xValues)
+    
 #print(prediction.round(2))
 #print(predict_train.round(2))
