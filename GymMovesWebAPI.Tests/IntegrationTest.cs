00892e9f3b8f5a18875091c94e3bde160c10c1dd
Longji Kang <derpyissunny@gmail.com>
2020-09-15 19:04:33 +0200

    Added integration testing

---
+++ GymMovesWebAPI.Tests/IntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using GymMovesWebAPI.Data.Models.RequestModels;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GymMovesWebAPI.Tests {
+    public class IntegrationTest : IClassFixture<WebApplicationFactory<Startup>> {
+        private readonly HttpClient httpClient;
+
+        public IntegrationTest(WebApplicationFactory<Startup> factory) {
+            httpClient = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task Test() {
+            Assert.True(true);
+        }
+    }
+}
