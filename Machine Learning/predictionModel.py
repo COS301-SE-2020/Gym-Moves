128d4ca4e8fe36593764935b66c32e0bb653aa1b
TiaMangena <u18059903@gmail.com>
2020-09-15 22:12:13 +0200

    updated prediction model parameters

--- Machine Learning/predictionModel.py
+++ Machine Learning/predictionModel.py
@@ -10,6 +10,14 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import joblib
 from firebase_admin import storage
+from datetime import timedelta
+
+def getCount7DaysAgo(dt,dates):
+    prevweek = dt - timedelta(days = 7)
+    for el in dates:
+        if el[0] == prevweek:
+            return el[1]
+    return 0
 
 def getData(gym, xValues, yValues):
     geturl = "https://gymmoveswebapi.azurewebsites.net/api/gymattendance/get"
@@ -17,20 +25,54 @@ def getData(gym, xValues, yValues):
     res = requests.get(url = geturl, params= getparams)
     data = json.loads(res.text)
     classes = getClasses(gym)
-    #k = 0
+    dates = list()
+    for d in data:
+        dt = datetime.datetime(d['year'], d['month'], d['day'])
+        temp = list()
+        temp.append(dt)
+        temp.append(d['count'])
+        dates.append(temp)
+
     
     for d in data:
         dt = datetime.datetime(d['year'], d['month'], d['day'])
         dayOfWeek = dt.isoweekday()
+        prev = getCount7DaysAgo(dt,dates)
         #print(dayOfWeek)
         lis = list()
         lis.append(dayOfWeek)
         (h,m) = d['time'].split(':')
-        intTime = int(h) * 3600 + int(m) * 60
+        intTime = int(h) - 6
+        #intTime = int(h) * 3600 + int(m) * 60
         lis.append(intTime)
-        #print(lis)
+        if prev != 0:
+            lis.append(prev)
+            yValues.append(d['count'])
+        else:
+            lis.append(5)
+            yValues.append(10)
         xValues.append(lis)
-        yValues.append(d['count'])
+
+        #again+++++++++++++++++++
+    for d in data:
+        dt = datetime.datetime(d['year'], d['month'], d['day'])
+        dayOfWeek = dt.isoweekday()
+        prev = getCount7DaysAgo(dt,dates)
+        #print(dayOfWeek)
+        lis = list()
+        lis.append(dayOfWeek)
+        (h,m) = d['time'].split(':')
+        intTime = int(h) - 6
+        #intTime = int(h) * 3600 + int(m) * 60
+        lis.append(intTime)
+        if prev != 0:
+            lis.append(int(prev/10))
+            yValues.append(int(d['count']/10))
+        else:
+            lis.append(10)
+            yValues.append(12)
+        xValues.append(lis)
+            
     #print(xValues)
     #print(yValues)
 
@@ -55,8 +97,11 @@ getData(3, xValues, yValues)
 print(xValues)
 print(yValues)
 
-mlp = MLPRegressor(solver= 'lbfgs', learning_rate='adaptive', shuffle = False)
+mlp = MLPRegressor(solver= 'lbfgs', activation= 'relu', shuffle = False, learning_rate_init = 0.015, max_iter = 250)
 mlp.fit(xValues,yValues)
+
+print(mlp.predict(xValues))
+print(mlp.predict([[1,5,20]]))
 
 if (not len(firebase_admin._apps)):
 	cred = credentials.Certificate(r'Machine Learning\sdk.json')
@@ -70,7 +115,7 @@ b = bucket.blob('model-v/model.joblib')
 b.upload_from_filename('model.joblib')
 print('model uploaded!')
 
-predict_train = mlp.predict(xValues)
-prediction = mlp.predict([[1,72000]])
-print(prediction.round(0))
-print(predict_train.round(0))
+#predict_train = mlp.predict(xValues)
+#prediction = mlp.predict([[1,72000]])
+#print(prediction.round(0))
+#print(predict_train.round(0))
